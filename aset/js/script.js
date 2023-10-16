e0e7f1765f502c4c9c480656aa69e0f737ebf00f
Hadid20 <gustihadid56@gmail.com>
2023-10-16 16:28:30 +0700

    h1

---
+++ aset/js/script.js
@@ -0,0 +1,10 @@
+var hasil = document.querySelector('.hasil > input');
+var Ksatu = document.querySelector('.sistem > .sistemsatu')
+var Kdua = document.querySelector('.sistem > .sistemdua')
+
+var btn =document.querySelector('.btn button')
+
+function kali(){
+
+}
+
